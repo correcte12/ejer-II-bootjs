4bed2c54c46cf1d7db24996905a2b1c7b56fa281
correcte12 <correcte12@gmail.com>
2022-11-12 21:29:24 +0100

    fin ejercicio 7

--- index.js
+++ index.js
@@ -42,3 +42,128 @@ function ejercicioTres () {
         document.getElementById("resultado").innerHTML = "El perímetro del rectángulo es: " + perimetro;
     }
 }
+
+function ejercicioCuatro(comprobarPar) {
+    var comprobarPar = prompt("Dime el número a comprobar", 0);
+ if (comprobarPar == 0) {
+     alert("No has introducido ningún número");
+
+ }
+ if (comprobarPar > 0 ) {
+    if (comprobarPar%2==0) {
+        alert ("El número es par")
+    }
+    else {
+        alert ("El número es impar");
+    }
+ }
+ }
+
+ function ejercicioCincoUno(){
+    
+    let personas = [
+        { nombre: 'Juan', edad: 18 },
+        { nombre: 'María', edad: 16 },
+        { nombre: 'Pedro', edad: 20 },
+        { nombre: 'Pablo', edad: 15 },
+        { nombre: 'Laura', edad: 19 },
+      ];
+
+    let mayores = [];
+    
+    for (let i = 0; i<personas.length; i++) {
+       //console.log(personas[i].edad)
+        var currentNumber = personas[i].edad;
+        if (currentNumber > 17) {
+          mayores.push(personas[i].nombre)
+          
+        }
+    };
+    console.log("Los siguientes nombres son mayores de edad: " + mayores);
+ }
+
+ function ejercicioCincoDos(){
+    
+    let personas = [
+        { nombre: 'Juan', edad: 18 },
+        { nombre: 'María', edad: 16 },
+        { nombre: 'Pedro', edad: 20 },
+        { nombre: 'Pablo', edad: 15 },
+        { nombre: 'Laura', edad: 19 },
+      ];
+    
+    let edadMayor= 0;
+    let elMayor = "";
+    
+    for (let i = 0; i<personas.length; i++) {
+        var currentNumber = personas[i].edad;
+        if (currentNumber > edadMayor) {
+          edadMayor = currentNumber;  
+          elMayor= (personas[i].nombre);
+          
+        }
+    };
+    console.log("El más mayor de todos es: " + elMayor);
+ }
+
+ function ejercicioCincoTres(){
+    
+    let personas = [
+        { nombre: 'Juan', edad: 18 },
+        { nombre: 'María', edad: 16 },
+        { nombre: 'Pedro', edad: 20 },
+        { nombre: 'Pablo', edad: 15 },
+        { nombre: 'Laura', edad: 19 },
+      ];
+    
+    let edadJoven= 1000;
+    let elJoven = "";
+    
+    for (let i = 0; i<personas.length; i++) {
+        var currentNumber = personas[i].edad;
+        if (currentNumber < edadJoven) {
+            edadJoven = currentNumber;  
+            elJoven= (personas[i].nombre);
+          
+        }
+    };
+    console.log("El más joven de todos es: " + elJoven);
+ }
+ 
+ function ejercicioSeisUno(){
+    const pokemons = [
+        {nombre:'Garchomp', tipoDePokemon:'Tierra' },
+        {nombre:'Kyogre', tipoDePokemon:'Agua'},
+        {nombre:'Charmander', tipoDePokemon:'Fuego'},
+        {nombre:'Arbok', tipoDePokemon:'Fuego'},
+    ];
+
+    let listaPokemons = [];
+
+     for (let i = 0; i < pokemons.length; i++) {
+        listaPokemons.push(pokemons[i].nombre)
+
+         }
+     console.log(listaPokemons)
+     };
+
+     function ejercicioSeisDos(){
+        const pokemons = [
+            {nombre:'Garchomp', tipoDePokemon:'Tierra' },
+            {nombre:'Kyogre', tipoDePokemon:'Agua'},
+            {nombre:'Charmander', tipoDePokemon:'Fuego'},
+            {nombre:'Arbok', tipoDePokemon:'Fuego'},
+        ];
+    
+        let listaPokemonsFuego = [];
+    
+         for (let i = 0; i < pokemons.length; i++) {
+            var tipoPokemon = pokemons[i].tipoDePokemon
+            if (tipoPokemon == "Fuego") {
+                listaPokemonsFuego.push(pokemons[i].nombre)
+            }
+            
+    
+             }
+         console.log(listaPokemonsFuego)
+         };
